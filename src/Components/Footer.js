5e99a2ea9156bbb8e9c28ec1a4b3e993e04494c7
Prajvals1211 <121630883+Prajvals1211@users.noreply.github.com>
2023-08-26 20:55:30 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,16 +7,16 @@ export const Footer = () => {
     <div className="py-6 ml-2 flex bg-pink-50 items-center flex-col">
       <div className="flex items-center">
         <span className=" text-black ml-[50px]">
-          Developed by :<span className="font-bold"> {user.name}</span>
+          Developed by :<span className="text-red-600 font-bold"> {user.name}</span>
         </span>
         {/* <span>Email : {user.email}</span> */}
       </div>
       <div className="flex justify-between">
         <span className="m-2 mr-[80px]">
-          Email :<span className="font-semibold">{user.email}</span>
+          Email :<span className=" m-1 font-semibold">{user.email}</span>
         </span>
         <span className="m-2 ml-[70px]"> Contact :
-        <span className=" font-semibold">{user.contact}</span></span>
+        <span className="m-1 font-semibold">{user.contact}</span></span>
       </div>
     </div>
   );
